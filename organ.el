1da2d041170fcf3d90554ec405d2c7f25cee621c
Clancy Rowley <cwrowley@princeton.edu>
2025-07-13 14:32:33 -0400

    Add keymap organ-map

--- organ.el
+++ organ.el
@@ -3,4 +3,14 @@
 (require 'organ-churches)
 (require 'organ-gigs)
 
+(defvar organ-map
+  (let ((map (make-sparse-keymap)))
+    (define-key map (kbd "g") 'organ-gigs)
+    (define-key map (kbd "p") 'organ-pieces)
+    (define-key map (kbd "a") 'organ-add-gig)
+    map)
+  "Keymap for organ commands")
+
+(global-set-key (kbd "C-c o") organ-map)
+
 (provide 'organ)
